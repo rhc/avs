497f6e33a4b251ec901586b9019b8d24fd4e23f0
Christian Kyony <ckyony@changamuka.com>
2024-03-14 19:23:12 +0200

    Refactor after change to ~/.rubocop.yml

--- lib/asset/command.rb
+++ lib/asset/command.rb
@@ -2,58 +2,74 @@
 
 require_relative 'model'
 
-# rubocop:disable Metrics/MethodLength
-def fetch_fixtures
-  [
-    CmdbAsset.new(
-      id: 100,
-      country_code: 'cd',
-      business_unit: 'bu',
-      sub_area: 'sa',
-      application: 'ewallet',
-      utr: 'UTR01966',
-      fqdn: 'fqdn.co.za',
-      host_name: 'fqdn',
-      ip_address: '172.16.19.66',
-      operating_system: 'ubuntu',
-      server_environment: 'linux',
-      server_category: 'linux',
-      host_key: 'xxxx',
-      country: 'DRC'
-    ),
-    CmdbAsset.new(
-      id: 200,
-      country_code: 'mw',
-      business_unit: 'bu',
-      sub_area: 'sa',
-      application: 'atm',
-      utr: 'UTR01966',
-      fqdn: 'fqdn.co.za',
-      host_name: 'fqdn',
-      ip_address: '198.172.19.66',
-      operating_system: 'ubuntu',
-      server_environment: 'linux',
-      server_category: 'linux',
-      host_key: 'xxxx',
-      country: 'Malawi'
-    ),
-    CmdbAsset.new(
-      id: 300,
-      country_code: 'bw',
-      business_unit: 'bu',
-      sub_area: 'sa',
-      application: 'forex',
-      utr: 'UTR01986',
-      fqdn: 'fqdn.co.za',
-      host_name: 'fqdn',
-      ip_address: '10.12.19.66',
-      operating_system: 'ubuntu',
-      server_environment: 'linux',
-      server_category: 'linux',
-      host_key: 'xxxx',
-      country: 'Botswana'
-    )
-  ]
-end
+class App
+  extend GLI::App
 
-# rubocop:enable Metrics/MethodLength
+  desc 'Manage assets'
+  arg_name 'Describe arguments to asset here'
+  command :assets do |c|
+    c.desc 'Describe a switch to asset'
+    c.switch :s
+
+    c.desc 'Describe a flag to asset'
+    c.default_value 'default'
+    c.flag :f
+    c.action do |_global_options, _options, _args|
+      # Your command logic specific to 'asset' here
+      assets = [
+        CmdbAsset.new(
+          id: 100,
+          country_code: 'cd',
+          business_unit: 'bu',
+          sub_area: 'sa',
+          application: 'ewallet',
+          utr: 'UTR01966',
+          fqdn: 'fqdn.co.za',
+          host_name: 'fqdn',
+          ip_address: '172.16.19.66',
+          operating_system: 'ubuntu',
+          server_environment: 'linux',
+          server_category: 'linux',
+          host_key: 'xxxx',
+          country: 'DRC'
+        ),
+        CmdbAsset.new(
+          id: 200,
+          country_code: 'mw',
+          business_unit: 'bu',
+          sub_area: 'sa',
+          application: 'atm',
+          utr: 'UTR01966',
+          fqdn: 'fqdn.co.za',
+          host_name: 'fqdn',
+          ip_address: '198.172.19.66',
+          operating_system: 'ubuntu',
+          server_environment: 'linux',
+          server_category: 'linux',
+          host_key: 'xxxx',
+          country: 'Malawi'
+        ),
+        CmdbAsset.new(
+          id: 300,
+          country_code: 'bw',
+          business_unit: 'bu',
+          sub_area: 'sa',
+          application: 'forex',
+          utr: 'UTR01986',
+          fqdn: 'fqdn.co.za',
+          host_name: 'fqdn',
+          ip_address: '10.12.19.66',
+          operating_system: 'ubuntu',
+          server_environment: 'linux',
+          server_category: 'linux',
+          host_key: 'xxxx',
+          country: 'Botswana'
+        )
+      ]
+      puts 'list assets'
+      assets.each do |asset|
+        puts asset
+      end
+    end
+  end
+end
